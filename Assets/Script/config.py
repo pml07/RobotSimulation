fcc9b83fee9dfb8adbfcc1bd0b4386161fcaf6bb
ivanchiou <wyattkidd@gmail.com>
2023-03-02 10:50:39 +0800

    fixed .env port from string to int

--- Assets/Script/config.py
+++ Assets/Script/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 ### socket
 UDP_IP = os.getenv('UDP_IP','127.0.0.1')
-UDP_SEND_PORT = os.getenv('UDP_SEND_PORT', 5060)
-UDP_RECEIVE_PORT = os.getenv('UDP_RECEIVE_PORT', 5061)
+UDP_SEND_PORT = int(os.getenv('UDP_SEND_PORT', 5060))
+UDP_RECEIVE_PORT = int(os.getenv('UDP_RECEIVE_PORT', 5061))
 ROS_API_BASE_URL = os.getenv('ROS_API_BASE_URL','http://localhost')
 IMPORT_FILE_NAME = os.getenv('IMPORT_FILE_NAME','../Data/angle_0.json')
 
