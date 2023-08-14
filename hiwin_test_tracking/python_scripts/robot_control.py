00215b620109cda4ff369eabfd4af5c400c8ef4c
pml07 <81510945+pml07@users.noreply.github.com>
2023-08-14 10:17:38 +0800

    left hand

--- hiwin_test_tracking/python_scripts/robot_control.py
+++ hiwin_test_tracking/python_scripts/robot_control.py
@@ -3,8 +3,6 @@ import mediapipe as mp
 import socket
 import numpy as np
 import math
-import time
-
 
 ### socket
 UDP_IP = '127.0.0.1'
@@ -17,7 +15,6 @@ send_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 receive_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 receive_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 receive_socket.bind((UDP_IP, UDP_RECEIVE_PORT))
-
 
 angle1, a1_last, a1_f = 0, 0, 0
 angle2, a2_last, a2_f = 0, 0, 0
@@ -45,7 +42,7 @@ def angle(v1, v2):
   return math.acos(dotproduct(v1, v2) / (length(v1)*length(v2)))
 
 
-### mediapipe
+## mediapipe
 cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
 mpPose = mp.solutions.pose
 pose = mpPose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5, static_image_mode=False, model_complexity=1)
@@ -59,9 +56,9 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 影像高度
 
 
 
-### save realtime video
+## save realtime video
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter('0302_0.avi', fourcc, 5.0, (width, height))
+out = cv2.VideoWriter('0715_2.avi', fourcc, 15.0, (width, height))
 while True:
   ret, img = cap.read()
   if not ret:
@@ -81,62 +78,65 @@ while True:
       point_list.append(round(float(data_point.z), 3))
       point_list.append(round(float(data_point.visibility), 3))
       joint_list.append(point_list)
-    send_switch = 0
-    
-    
-    # 11: lshoulder / 12: rshoulder / 14: elbow / 16: wrist / 22: thumb / 18: pinky / 20: index / 24: hip
+    send_1 = 0
+    send_2 = 0
+    send_3 = 0
+    send_4 = 0
+    send_5 = 0
+
+    # 11: lshoulder / 12: rshoulder / 13: elbow / 15: wrist / 21: thumb / 17: pinky / 19: index / 23: hip
     # 肩膀->手肘
-    arm = (joint_list[14][0]-joint_list[12][0], joint_list[14][1]-joint_list[12][1], joint_list[14][2]-joint_list[12][2])
+    arm = (joint_list[13][0]-joint_list[12][0], joint_list[13][1]-joint_list[12][1], joint_list[13][2]-joint_list[12][2])
     # 手肘->手腕
-    forearm = (joint_list[16][0]-joint_list[14][0], joint_list[16][1]-joint_list[14][1], joint_list[16][2]-joint_list[14][2])
+    forearm = (joint_list[15][0]-joint_list[13][0], joint_list[15][1]-joint_list[13][1], joint_list[15][2]-joint_list[13][2])
     # 左右肩
-    shoulder = (joint_list[12][0]-joint_list[11][0], joint_list[12][1]-joint_list[11][1], joint_list[12][2]-joint_list[11][2])
+    shoulder = (joint_list[11][0]-joint_list[12][0], joint_list[11][1]-joint_list[12][1], joint_list[11][2]-joint_list[12][2])
     # 食指
-    index = (joint_list[20][0]-joint_list[16][0], joint_list[20][1]-joint_list[16][1], joint_list[20][2]-joint_list[16][2])
+    index = (joint_list[19][0]-joint_list[15][0], joint_list[19][1]-joint_list[15][1], joint_list[19][2]-joint_list[15][2])
     # 小指
-    pinky = (joint_list[18][0]-joint_list[16][0], joint_list[18][1]-joint_list[16][1], joint_list[18][2]-joint_list[16][2])
+    pinky = (joint_list[17][0]-joint_list[15][0], joint_list[17][1]-joint_list[15][1], joint_list[17][2]-joint_list[15][2])
     # 手肘->食指 
-    elbow_index = (joint_list[20][0]-joint_list[14][0], joint_list[20][1]-joint_list[14][1], joint_list[20][2]-joint_list[14][2])
+    elbow_index = (joint_list[19][0]-joint_list[13][0], joint_list[19][1]-joint_list[13][1], joint_list[19][2]-joint_list[13][2])
     # 手肘->拇指 
-    elbow_thumb = (joint_list[22][0]-joint_list[14][0], joint_list[22][1]-joint_list[14][1], joint_list[22][2]-joint_list[14][2])
+    elbow_thumb = (joint_list[21][0]-joint_list[13][0], joint_list[21][1]-joint_list[13][1], joint_list[21][2]-joint_list[13][2])
     # 肩膀->骨盆
-    hip_shou = (joint_list[12][0]-joint_list[24][0], joint_list[12][1]-joint_list[24][1], joint_list[12][2]-joint_list[24][2])
+    hip_shou = (joint_list[12][0]-joint_list[23][0], joint_list[12][1]-joint_list[23][1], joint_list[12][2]-joint_list[23][2])
 
 
     #### calculate angle
-    if joint_list[12][3] > 0.8 and joint_list[14][3] > 0.8 and joint_list[16][3] > 0.8:
-      pinky_index = cross(pinky, index)
+    if joint_list[12][3] > 0.8 and joint_list[13][3] > 0.8 and joint_list[15][3] > 0.8:
+      index_pinky = cross(index, pinky)
       arm_fore = cross((-arm[0],-arm[1], -arm[2]), forearm)
       
-      # J1角度: -165~+165
-      J1 = round(math.degrees(angle((arm[0], 0, arm[2]), (-1, 0, 0))), 1)
+      # J1角度
+      J1 = round(math.degrees(angle((arm[0], 0, arm[2]), (1, 0, 0))), 3)
       # J1方向 
-      dir_a1 = dotproduct((0, 0, arm[2]), (0,0,-1))
+      dir_a1 = dotproduct((0, 0, arm[2]), (0, 0, 1))
       if dir_a1 != 0:
         dir_a1 /= abs(dir_a1)
         angle1 = J1*dir_a1
       else:
         angle1 = J1*dir_a1
-
-      if angle1 > 165:
-        angle1 = 165
-      elif angle1 < -165:
-        angle1 = -165
+      # J1: -165~+165
+      if angle1 > 163:
+        angle1 = 163
+      elif angle1 < -163:
+        angle1 = -163
       else:
         pass
       if abs(abs(angle1)-abs(a1_last)) <= 1:
         pass
       else:
         a1_f = angle1
-        send_switch +=1
+        send_1 += 1
       a1_last = angle1
 
       # J2角度: -125 ~ +85->70
       J2 = round(math.degrees(angle((shoulder[0], shoulder[1], 0),(arm[0], arm[1], 0))), 1)
       # J2方向
-      if joint_list[14][1] > joint_list[12][1]:
+      if joint_list[13][1] > joint_list[12][1]:
         angle2 = -J2
-      elif joint_list[14][1] <= joint_list[12][1]:
+      elif joint_list[13][1] <= joint_list[12][1]:
         angle2 = J2
 
       if angle2 > 70:
@@ -146,21 +146,19 @@ while True:
       else:
         pass
       if abs(abs(angle2)-abs(a2_last)) <= 1:
-        pass
+        send_2 = 0
       else:
         a2_f = angle2
-        send_switch +=1
+        send_2 += 1
       a2_last = angle2
 
       # J3角度: -55 ~ +185
       J3 = round(math.degrees(angle((arm[0], arm[1], 0), (forearm[0], forearm[1], 0))), 1)
       # J3方向
-      if joint_list[16][1] < joint_list[14][1]:
+      if joint_list[15][1] < joint_list[13][1]:
         angle3 = J3 + 90
-      elif joint_list[16][1] >= joint_list[14][1]:
+      elif joint_list[15][1] >= joint_list[13][1]:
         angle3 = -J3 + 90
-      if 80 <= angle3 <= 100:
-        angle3 = 90
 
       if angle3 > 180:
         angle3 = 180
@@ -169,80 +167,76 @@ while True:
       else:
         pass
       if abs(abs(angle3)-abs(a3_last)) <= 1:
-        pass
+        send_3 = 0
       else:
         a3_f = angle3
-        send_switch +=1
+        send_3 += 1
       a3_last = angle3
       
       # J4角度: -190 ~ +190
-      J4 = round(math.degrees(angle(pinky_index, arm_fore)), 1)
+      J4 = round(math.degrees(angle(index_pinky, arm_fore)), 1)
       # J4方向
       if J4 == 0:
         angle4 = -J4
       elif J4 == 180:
         angle4 = J4
       else:  
-        cross_hand_elb = cross(pinky_index,arm_fore)
+        cross_hand_elb = cross(index_pinky, arm_fore)
         dir_a4 = dotproduct(cross_hand_elb, forearm)
         dir_a4 /= abs(dir_a4)
         angle4 = J4*dir_a4
 
-      if angle4 > 190:
-        angle4 = 190
-      elif angle4 < -190:
-        angle4 = -190
+      if angle4 > 90:
+        angle4 = 90
+      elif angle4 < -90:
+        angle4 = -90
       else:
         pass
       if abs(abs(angle4)-abs(a4_last)) <= 1:
         pass
       else:
         a4_f = angle4
-        send_switch +=1
+        send_4 += 1
       a4_last = angle4
       
-      # J5角度: -116 ~ +116
+      # J5角度: -115 ~ +115
       J5 = round(math.degrees(angle(forearm, index)),1)
       # J5方向
-      if joint_list[20][1] >= joint_list[16][1]:
+      if joint_list[19][1] >= joint_list[15][1]:
         angle5 = -J5
-      elif joint_list[20][1] < joint_list[16][1]:
+      elif joint_list[19][1] < joint_list[15][1]:
         angle5 = J5
 
-      if angle5 > 116:
-        angle5 = 116
-      elif angle5 < -116:
-        angle5 = -116
+      if angle5 > 110:
+        angle5 = 110
+      elif angle5 < -110:
+        angle5 = -110
       else:
         pass
       if abs(abs(angle5)-abs(a5_last)) <= 1:
         pass
       else:
         a5_f = angle5
-        send_switch +=1
+        send_5 += 1
       a5_last = angle5
 
       # J6
-      pinky_index = cross(pinky, index)
-      angle6 = round(math.degrees(angle((0, pinky_index[1], pinky_index[2]), (0, 0, 1))),1)
-      if abs(abs(angle6)-abs(a6_last)) <= 1:
-        pass
-      else:
-        a6_f = angle6
-        send_switch +=1
+      angle6 = 0
+      a6_f = angle6
       a6_last = angle6
 
     send_ = str(a1_f)+';'+str(a2_f)+';'+str(a3_f)+';'+str(a4_f)+';'+str(a5_f)+';'+str(a6_f)
-    # print(send_)
-    if send_switch > 0:
+    if send_2 and send_3 and send_5 > 0:
       send_socket.sendto((str(send_)).encode(), (UDP_IP,UDP_SEND_PORT))
-
-      message, address = receive_socket.recvfrom(1024)
-      msg = message.decode().split(";")
-      jRot = [msg[0], msg[1], msg[2], msg[3], msg[4], msg[5]]
-      jPos = [msg[6], msg[7], msg[8], msg[9], msg[10], msg[11]]
-      rpm = [msg[12], msg[13], msg[14], msg[15], msg[16], msg[17]]
-      torque = [msg[18], msg[19], msg[20], msg[21], msg[22], msg[23]]
+      print('-----send:', send_)
+      send_switch = 0
+      # message, address = receive_socket.recvfrom(1024)
+      # msg = message.decode().split(";")
+      # jRot = [msg[0], msg[1], msg[2], msg[3], msg[4], msg[5]]
+      # print("receive: ", jRot)
+      with open('0715_2.txt', 'a') as file:
+        file.write(send_)
+        file.write('\n')
 
   out.write(img)
   cv2.imshow('Robot Arm', img)
