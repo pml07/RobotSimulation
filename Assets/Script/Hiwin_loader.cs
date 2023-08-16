e4ff3c992171fae7526eadde596bfe1a0ad2c2c7
pml07 <81510945+pml07@users.noreply.github.com>
2023-08-16 21:39:33 +0800

    2 different robots control in Unity

---
+++ Assets/Script/Hiwin_loader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class Hiwin_loader
+{
+    // joint list define: [j1, j2, j3, j4, j5, j6]
+    public static string coordinate_txt_path = @"Assets/DataSet/Demo/6to6_demo/predict_demo.txt";
+    public static string[] coordinate_lines = File.ReadAllLines(coordinate_txt_path);
+    public static float[,] coordinate_list = new float[coordinate_lines.Length, 6];
+
+    public void txt_reader()
+    {
+        int count = 0;
+        foreach (var item in coordinate_lines)
+        {
+            string[] subs = item.Split(',');
+            coordinate_list[count, 0] = (float)Convert.ToDouble(subs[0]);
+            coordinate_list[count, 1] = (float)Convert.ToDouble(subs[1]);
+            coordinate_list[count, 2] = (float)Convert.ToDouble(subs[2]);
+            coordinate_list[count, 3] = (float)Convert.ToDouble(subs[3]);
+            coordinate_list[count, 4] = (float)Convert.ToDouble(subs[4]);
+            coordinate_list[count, 5] = (float)Convert.ToDouble(subs[5]);
+            Debug.Log("---------------");
+            Debug.Log(coordinate_list[count, 0] + "," + coordinate_list[count, 1] + "," + coordinate_list[count, 2] + "," + coordinate_list[count, 3] + "," + coordinate_list[count, 4] + "," + coordinate_list[count, 5]);
+            
+            count += 1;
+        }
+        count = 0;   
+    }
+}
---
+++ Assets/Script/Rotate_hiwin.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using static Hiwin_loader;
+
+
+public class Rotate_hiwin : MonoBehaviour
+{
+    // joint
+    public Transform baseJoint;
+    public Transform upperbaseJoint;
+    public Transform torsoJoint;
+    public Transform shoulderJoint;
+    public Transform upperarmJoint;
+    public Transform armJoint;
+    public Transform elbowJoint;
+    public Transform forearmJoint;
+    public Transform wristJoint;
+    public Transform handJoint;
+
+    public Vector3[] axisPositions = new Vector3[6];
+
+    private string dataFilePath;
+
+    Hiwin_loader hiwin_data = new Hiwin_loader();
+
+    public int count;
+
+    void Start()
+    {
+        dataFilePath = Application.dataPath + "/DataSet/Demo/6to6_demo/predict_demo.json";
+        count = 0;
+        hiwin_data.txt_reader();
+    }
+
+    void Update()
+    {       
+        if (count < Hiwin_loader.coordinate_list.Length)
+        {
+            upperbaseJoint.localRotation = Quaternion.Euler(0, Hiwin_loader.coordinate_list[count, 0], 0);  // 0 a1 0
+            torsoJoint.localRotation = Quaternion.Euler(Hiwin_loader.coordinate_list[count, 1], 0, 0);  // a2 0 0
+            armJoint.localRotation = Quaternion.Euler(Hiwin_loader.coordinate_list[count, 2], 0, 0);  // a3 0 0
+            forearmJoint.localRotation = Quaternion.Euler(0, Hiwin_loader.coordinate_list[count, 3], 0);  // 0 a4 0
+            wristJoint.localRotation = Quaternion.Euler(Hiwin_loader.coordinate_list[count, 4], 0, 0);  // a5 0 0
+            handJoint.localRotation = Quaternion.Euler(0, Hiwin_loader.coordinate_list[count, 5], 0);  // 0 a6 0
+            Debug.Log(Hiwin_loader.coordinate_list[count, 0] + "," + Hiwin_loader.coordinate_list[count, 1] + "," + Hiwin_loader.coordinate_list[count, 2] + "," + Hiwin_loader.coordinate_list[count, 3] + "," + Hiwin_loader.coordinate_list[count, 4] + "," + Hiwin_loader.coordinate_list[count, 5]);
+            count += 1;
+        }
+
+        else if (count == Hiwin_loader.coordinate_list.Length)
+        {
+            upperbaseJoint.localRotation = Quaternion.Euler(0, Hiwin_loader.coordinate_list[count, 0], 0);
+            torsoJoint.localRotation = Quaternion.Euler(Hiwin_loader.coordinate_list[count, 1], 0, 0);
+            armJoint.localRotation = Quaternion.Euler(Hiwin_loader.coordinate_list[count, 2], 0, 0);
+            forearmJoint.localRotation = Quaternion.Euler(0, Hiwin_loader.coordinate_list[count, 3], 0);
+            wristJoint.localRotation = Quaternion.Euler(Hiwin_loader.coordinate_list[count, 4], 0, 0);
+            handJoint.localRotation = Quaternion.Euler(0, Hiwin_loader.coordinate_list[count, 5], 0);
+
+            count += 0;
+        }
+
+    //    jPosition();
+    //    string jointDataJson = JsonUtility.ToJson(new JointData { j1x = axisPositions[0].x, j1y = axisPositions[0].y, j1z = axisPositions[0].z, 
+    //                                                            j2x = axisPositions[1].x, j2y = axisPositions[1].y, j2z = axisPositions[1].z, 
+    //                                                            j3x = axisPositions[2].x, j3y = axisPositions[2].y, j3z = axisPositions[2].z, 
+    //                                                            j4x = axisPositions[3].x, j4y = axisPositions[3].y, j4z = axisPositions[3].z, 
+    //                                                            j5x = axisPositions[4].x, j5y = axisPositions[4].y, j5z = axisPositions[4].z,
+    //                                                            j6x = axisPositions[5].x, j6y = axisPositions[5].y, j6z = axisPositions[5].z });
+    //    File.AppendAllText(dataFilePath, jointDataJson + "\n");
+    }
+
+    void jPosition()
+    {
+        axisPositions[0] = baseJoint.transform.position - baseJoint.transform.position;
+        axisPositions[1] = upperarmJoint.transform.position - baseJoint.transform.position;
+        axisPositions[2] = elbowJoint.transform.position - baseJoint.transform.position;
+        axisPositions[3] = forearmJoint.transform.position - baseJoint.transform.position;
+        axisPositions[4] = wristJoint.transform.position - baseJoint.transform.position;
+        axisPositions[5] = handJoint.transform.position - baseJoint.transform.position;
+        // Debug.Log(axisPositions[0] + "," + axisPositions[1] + "," + axisPositions[2] + "," + axisPositions[3] + "," + axisPositions[4] + "," + axisPositions[5]);
+        // Debug.Log(baseJoint.transform.position.ToString("F3") + "," + upperbaseJoint.transform.position.ToString("F3") + "," + torsoJoint.transform.position.ToString("F3") + "," + shoulderJoint.transform.position.ToString("F3") + "," + upperarmJoint.transform.position.ToString("F3") + "," + armJoint.transform.position.ToString("F3") + "," + elbowJoint.transform.position.ToString("F3") + "," + forearmJoint.transform.position.ToString("F3") + "," + wristJoint.transform.position.ToString("F3") + "," + handJoint.transform.position.ToString("F3"));
+    }
+
+    [System.Serializable]
+    private class JointData
+    {
+        public float j1x;
+        public float j1y;
+        public float j1z;
+        public float j2x;
+        public float j2y;
+        public float j2z;
+        public float j3x;
+        public float j3y;
+        public float j3z;
+        public float j4x;
+        public float j4y;
+        public float j4z;
+        public float j5x;
+        public float j5y;
+        public float j5z;
+        public float j6x;
+        public float j6y;
+        public float j6z;
+    }
+}
